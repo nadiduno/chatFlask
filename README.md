68f7e8c846f278681804478fbdeffeccce7ca18f
Nadi Duno <nadiduno@gmail.com>
2023-07-29 01:01:06 -0300

    Funcionalidade

---
+++ README.md
@@ -0,0 +1,8 @@
+
+Bibliotecas
+
+[pip install Flask](https://pypi.org/project/Flask/)
+
+[pip install python-socketio](https://pypi.org/project/python-socketio/)
+[pip install Flask-SocketIO](https://pypi.org/project/Flask-SocketIO/)
+[pip install simple-websocket](https://pypi.org/project/simple-websocket/)
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+#Funcionalidade de enviar mensagem
+@socketio.on('message')
+def send_mensagem(message):
+    send(message, broadcast=True)
 
 #decorator
 @app.route('/')
 def homepage():
     return render_template("index.html")
 
-app.run()
+# app.run(debug=True) para desenvol
+#Intranet com ipconfig
+#socketio.run(app, host='192.168.100.3')
+socketio.run(app, host='localhost')
